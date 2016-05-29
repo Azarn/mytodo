42f47b2a99df0a0a14f93dac5fedf5726fc7077a
Azarn <Alex3296@yandex.ru>
2016-05-29 05:09:12 +0000

    Added simple admin interface

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo, Category, Tag
+
+
+class MembershipInline(admin.TabularInline):
+    model = Todo.tags.through
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'deadline'
+    list_display = ('text', 'category', 'user', 'deadline', 'is_done')
+    list_editable = ('is_done',)
+    list_filter = (
+        ('user', admin.RelatedOnlyFieldListFilter),
+        ('is_done', admin.BooleanFieldListFilter),
+    )
+    search_fields = ('text', 'user__username', 'category__name',)
+    inlines = [
+        MembershipInline
+    ]
+    exclude = ('tags', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'user')
+    list_display = ('name', 'user')
+    list_filter = (
+        ('user', admin.RelatedOnlyFieldListFilter),
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    fields = ('name', 'user', 'color')
+    list_display = ('colored_name', 'user', 'color')
+    list_filter = (
+        ('user', admin.RelatedOnlyFieldListFilter),
+    )
+
--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,41 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+
+
+def validate_color(value):
+    try:
+        int(value, 16)
+    except ValueError:
+        raise ValidationError('{0} is not a hex color!'.format(value))
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=64, db_index=True)
+    color = models.CharField(max_length=6, validators=[validate_color])
+
+    def colored_name(self):
+        return format_html('<span style="color: #{};">{}</span>', self.color, self.name)
+    colored_name.admin_order_field = 'name'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=256, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    text = models.CharField(max_length=256, db_index=True)
+    is_done = models.BooleanField(default=False, db_index=True)
+    deadline = models.DateTimeField(blank=True, db_index=True)
