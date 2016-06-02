ba6fe75cb04e323cb17c118299ea56c2601a8cf5
Azarn <Alex3296@yandex.ru>
2016-06-02 17:03:46 +0000

    Added some tests for category api

--- todo/tests.py
+++ todo/tests.py
@@ -1,17 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework import status
 
-from .models import Category, Todo
+from .models import Category, Todo, Profile
+from .views import CategoryDetail, CategoryList, TagDetail, TagList, TodoDetail, TodoList
 
 
 class DefaultCategoryTestCase(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create(username='1', password='2')
-
-    def tearDown(self):
-        Category.objects.all().delete()
-        Todo.objects.all().delete()
+        self.user = get_user_model().objects.create(username='user')
 
     def _check_exists(self):
         return Category.objects.filter(user=self.user, name=Category.DEFAULT_NAME).exists()
@@ -52,3 +52,100 @@ class DefaultCategoryTestCase(TestCase):
 
         todo.delete()
         self.assertFalse(self._check_exists())
+
+
+class ApiUserRestrictionCategoryListTestCase(TestCase):
+    def setUp(self):
+        self.my_user = get_user_model().objects.create(username='my_user')
+        Profile(user=self.my_user).save()
+        self.other_user = get_user_model().objects.create(username='other_user')
+        Profile(user=self.other_user).save()
+        self.my_category = Category.objects.create(user=self.my_user, name='Test my category')
+        self.other_category = Category.objects.create(user=self.other_user, name='Test my category')
+        self.factory = APIRequestFactory()
+        self.url = reverse('todo:category-list')
+        self.view = CategoryList.as_view()
+
+    def test_category_list_methods_not_authenticated(self):
+        request = self.factory.get(self.url, format='json')
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        request = self.factory.post(self.url, format='json')
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_category_list_get(self):
+        request = self.factory.get(self.url, format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'], [{'id': self.my_category.id, 'name': self.my_category.name}])
+
+    def test_category_list_create(self):
+        data = {'name': 'New category', 'id': 100, 'user': self.other_user.id}
+        request = self.factory.post(self.url, data, format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Category.objects.all().count(), 3)
+        new_category = Category.objects.get(pk=response.data['id'])
+        self.assertNotEqual(new_category.id, data['id'])
+        self.assertNotEqual(new_category.user_id, self.other_user.id)
+        self.assertEqual(new_category.name, data['name'])
+        self.assertEqual(response.data, {'id': new_category.id, 'name': data['name']})
+
+
+class ApiUserRestrictionCategoryDetailTestCase(TestCase):
+    def setUp(self):
+        self.my_user = get_user_model().objects.create(username='my_user')
+        Profile(user=self.my_user).save()
+        self.other_user = get_user_model().objects.create(username='other_user')
+        Profile(user=self.other_user).save()
+        self.my_category = Category.objects.create(user=self.my_user, name='Test my category')
+        self.other_category = Category.objects.create(user=self.other_user, name='Test my category')
+        self.factory = APIRequestFactory()
+        self.view = CategoryDetail.as_view()
+        self.urls = [reverse('todo:category-detail', args=(self.my_category.id,)),
+                     reverse('todo:category-detail', args=(self.other_category.id,))]
+
+    def test_category_detail_methods_not_authenticated(self):
+        for url in self.urls:
+            request = self.factory.get(url, format='json')
+            response = self.view(request)
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+            request = self.factory.put(url, format='json')
+            response = self.view(request)
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+            request = self.factory.delete(url, format='json')
+            response = self.view(request)
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_category_detail_get(self):
+        request = self.factory.get(self.urls[0], format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request, pk=self.my_category.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'id': self.my_category.id, 'name': self.my_category.name})
+        request = self.factory.get(self.urls[1], format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request, pk=self.other_category.id)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_category_detail_put(self):
+        data = {'id': 100, 'user': 100, 'name': "New category name"}
+        request = self.factory.put(self.urls[0], data, format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request, pk=self.my_category.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'id': self.my_category.id, 'name': data['name']})
+        self.my_category = Category.objects.get(id=self.my_category.id)
+        self.assertEqual(self.my_category.name, data['name'])
+        self.assertEqual(self.my_category.user.id, self.my_user.id)
+        request = self.factory.put(self.urls[1], data, format='json')
+        force_authenticate(request, self.my_user, self.my_user.auth_token)
+        response = self.view(request, pk=self.other_category.id)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.other_category = Category.objects.get(id=self.other_category.id)
+        self.assertNotEqual(self.other_category.name, data['name'])
+        self.assertEqual(self.other_category.user.id, self.other_user.id)
--- todo/urls.py
+++ todo/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from .views import CategoryList, CategoryDetail, TagList, TagDetail, TodoList, TodoDetail
 
 urlpatterns = [
-    url(r'^categories/$', CategoryList.as_view()),
-    url(r'^categories/(?P<pk>[0-9]+)/$', CategoryDetail.as_view()),
-    url(r'^tags/$', TagList.as_view()),
-    url(r'^tags/(?P<pk>[0-9]+)/$', TagDetail.as_view()),
+    url(r'^category/$', CategoryList.as_view(), name='category-list'),
+    url(r'^category/(?P<pk>[0-9]+)/$', CategoryDetail.as_view(), name='category-detail'),
+    url(r'^tag/$', TagList.as_view()),
+    url(r'^tag/(?P<pk>[0-9]+)/$', TagDetail.as_view()),
     url(r'^todo/$', TodoList.as_view()),
     url(r'^todo/(?P<pk>[0-9]+)/$', TodoDetail.as_view()),
 ]
